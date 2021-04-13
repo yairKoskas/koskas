bfaf7f733ab77a048aaad529bc3acd46d1bd4ba1
yair koskas <yairi2003@gmail.com>
2021-04-13 21:52:57 +0300

    added joke script

--- koskas/__init__.py
+++ koskas/__init__.py
@@ -1,4 +1,9 @@
+import requests
 __version__ = "0.1.0"
 __author__ = 'Yair Koskas'
 def greet():
     print('Hello, i\'m koskas and this is my python package!')
+def joke():
+    r = requests.get('http://api.icndb.com/jokes/random')
+    s = r.text.encode('utf-8')
+    print(s[s.find('Chuck'):s.rfind('.')])
