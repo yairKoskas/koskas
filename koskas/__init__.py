50aa583b74aacdc5f2cf3fd243e3177de1063711
yair koskas <yairi2003@gmail.com>
2021-04-17 07:37:42 +0300

    deleted unused class Koskas

--- koskas/__init__.py
+++ koskas/__init__.py
@@ -2,20 +2,15 @@ import requests
 import json
 import math
 
-
-__version__ = "0.1.9"
+__version__ = "0.2.1"
 __author__ = 'Yair Koskas'
 
 
-class Koskas:
-    def __init__(self):
-        pass
-
-    def speak(self):
-        print('Hello, I\'m Yair Koskas')
-        print('If you see this, you are a very special person!')
-        print('Because you are the first person to ever care about that joke of a python package i made!')
-        print('Have a nice day!')
+def speak():
+    print('Hello, I\'m Yair Koskas')
+    print('If you see this, you are a very special person!')
+    print('Because you are the first person to ever care about that joke of a python package i made!')
+    print('Have a nice day!')
 
 
 def greet():
@@ -25,7 +20,7 @@ def greet():
 def joke():
     r = requests.get('http://api.icndb.com/jokes/random')
     s = str(dict(json.loads(r.text)['value'])['joke'])
-    s = s.replace('&quot;','"')
+    s = s.replace('&quot;', '"')
     print(s)
     return 'There isn\'t a return value, only Chuck Norris'
 
@@ -39,7 +34,7 @@ def isprime(n):
         return False
     if n == 2:
         return True
-    for i in range(2,math.ceil(math.sqrt(n))+1):
-        if n%i == 0:
+    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+        if n % i == 0:
             return False
     return True
